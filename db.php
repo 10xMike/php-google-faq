818928df653e25622e2ebfe656409bcb07da087a
10xMike <michelegec@gmail.com>
2021-06-23 20:22:23 +0200

    Initial commit

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$question_set = [
+    $question1 => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+    $question2 => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+    $question3 => "Perché il mio account è associato a un paese?",
+    $question4 => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+    $question5 => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+];
+
+$answer_set = [
+    $answer1 => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+    $answer2 => "",
+    $answer3 => "",
+    $answer4 => "",
+    $answer5 => "",
+];
