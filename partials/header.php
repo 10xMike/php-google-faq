fdcd2f65dd2815ddb74d86aa0d514d29ddea22e9
10xMike <michelegec@gmail.com>
2021-06-28 00:58:39 +0200

    Add nav.php, include __DIR__

--- partials/header.php
+++ partials/header.php
@@ -4,7 +4,8 @@
             <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt=""> <a class="link" href="#">Privacy e Termini</a>
         </div>
         <div class="rightmenu">
-            
+
         </div>
     </div>
+    <?php include __DIR__ . '/nav.php' ?>
 </div>
