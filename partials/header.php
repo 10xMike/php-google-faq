c273223f0a051b2ebc5a08699dced3d97a464804
10xMike <michelegec@gmail.com>
2021-06-28 00:53:56 +0200

    Add font awesome CDN

---
+++ partials/header.php
@@ -0,0 +1,10 @@
+<div id="site_header">
+    <div class="top">
+        <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt=""> <a class="link" href="#">Privacy e Termini</a>
+        </div>
+        <div class="rightmenu">
+            
+        </div>
+    </div>
+</div>
